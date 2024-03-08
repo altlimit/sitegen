1d7774a200d0ce0f4c0b70812f8fd2edc0270542
faisal <faisalraja@users.noreply.github.com>
2024-03-08 15:12:03 -0600

    disable cgo

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Building..."
 rm -rf ./build
-GOOS=windows GOARCH=amd64 go build -o build/win/sitegen.exe
-GOOS=linux GOARCH=amd64 go build -o build/linux/sitegen
-GOOS=darwin GOARCH=amd64 go build -o build/darwin/sitegen
-GOOS=darwin GOARCH=arm64 go build -o build/darwin-arm64/sitegen
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/win/sitegen.exe
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/linux/sitegen
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/darwin/sitegen
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o build/darwin-arm64/sitegen
 cd build/win
 zip -rq ../win.zip . -x ".*"
 cd ../linux
