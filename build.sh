0c1e4d951dc63f374ddd4560764bd20ea5366e99
faisal <faisalraja@users.noreply.github.com>
2026-02-26 19:03:28 -0600

    build fix

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+set -e
 if [ -z "$VERSION" ]; then
     VERSION="dev"
 fi
 echo "Building version $VERSION..."
 rm -rf ./build
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o build/win/sitegen.exe
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o build/linux/sitegen
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o build/darwin/sitegen
-CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=$VERSION" -o build/darwin-arm64/sitegen
+CGO_ENABLED=1 CC="zig cc -target x86_64-windows-gnu" CXX="zig c++ -target x86_64-windows-gnu" GOOS=windows GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o build/win/sitegen.exe
+CGO_ENABLED=1 CC="zig cc -target x86_64-linux-gnu" CXX="zig c++ -target x86_64-linux-gnu" GOOS=linux GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o build/linux/sitegen
+CGO_ENABLED=1 CC="zig cc -target x86_64-macos-none" CXX="zig c++ -target x86_64-macos-none" GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.version=$VERSION" -o build/darwin/sitegen
+CGO_ENABLED=1 CC="zig cc -target aarch64-macos-none" CXX="zig c++ -target aarch64-macos-none" GOOS=darwin GOARCH=arm64 go build -ldflags "-X main.version=$VERSION" -o build/darwin-arm64/sitegen
 cd build/win
 zip -rq ../win.zip . -x ".*"
 cd ../linux
